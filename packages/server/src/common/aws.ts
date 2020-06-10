ae8add3b47aa6e7be5321aa9884659d8adf170bd
Gabriel Souza <gabrie.ssouza@gmail.com>
2020-06-10 04:19:19 -0300

    subscriptions working

--- packages/server/src/common/aws.ts
+++ packages/server/src/common/aws.ts
@@ -1,34 +1,15 @@
 import AWS from 'aws-sdk';
 
-// AWS.config.update({ region: 'us-east-1' });
-//
-// export const SNS = new AWS.SNS();
-//
-// export const publishToQueue = (queue: string, message: any) =>
-//   new Promise((resolve, reject) => {
-//     SNS.publish(
-//       {
-//         Message: JSON.stringify(message),
-//         TopicArn: `arn:aws:sns:us-east-1:444098062489:${queue}`,
-//       },
-//       err => {
-//         if (err) reject(err);
-//         resolve();
-//       },
-//     );
-//   });
+AWS.config.update({ region: 'us-east-1' });
 
-export const SNS = new AWS.SNS({
-  endpoint: 'http://127.0.0.1:4002',
-  region: 'us-east-1',
-});
+export const SNS = new AWS.SNS();
 
 export const publishToQueue = (queue: string, message: any) =>
   new Promise((resolve, reject) => {
     SNS.publish(
       {
         Message: JSON.stringify(message),
-        TopicArn: `arn:aws:sns:us-east-1:123456789012:${queue}`,
+        TopicArn: `arn:aws:sns:us-east-1:444098062489:${queue}`,
       },
       err => {
         if (err) reject(err);
@@ -36,3 +17,22 @@ export const publishToQueue = (queue: string, message: any) =>
       },
     );
   });
+//
+// export const SNS = new AWS.SNS({
+//   endpoint: 'http://127.0.0.1:4002',
+//   region: 'us-east-1',
+// });
+//
+// export const publishToQueue = (queue: string, message: any) =>
+//   new Promise((resolve, reject) => {
+//     SNS.publish(
+//       {
+//         Message: JSON.stringify(message),
+//         TopicArn: `arn:aws:sns:us-east-1:123456789012:${queue}`,
+//       },
+//       err => {
+//         if (err) reject(err);
+//         resolve();
+//       },
+//     );
+//   });
--- packages/server/src/handlers/graphql.ts
+++ packages/server/src/handlers/graphql.ts
@@ -1,27 +1,33 @@
-import { ApolloServer } from 'apollo-server-lambda';
+import { Server } from 'aws-lambda-graphql';
 import { applyMiddleware } from 'graphql-middleware';
 
+import { connectionManager, eventProcessor, subscriptionManager } from '../common/subscriptions';
 import { MONGO_URL, getConnection } from '../common';
 import { dataloadersMiddleware } from '../graphql/loaders';
 import schema from '../graphql/schema';
 import loggedUserMiddleware from '../graphql/modules/user/loggedUserMiddleware';
 
-const server = new ApolloServer({
+const server = new Server({
   schema: applyMiddleware(schema, loggedUserMiddleware, dataloadersMiddleware),
   context: async req => {
     const dbConnection = await getConnection(MONGO_URL);
     return { ...req, dbConnection };
   },
-  // playground: {
-  //   endpoint: 'https://e07p5jga5e.execute-api.us-east-1.amazonaws.com/dev',
-  //   subscriptionEndpoint: 'wss://qi4dcv7dn5.execute-api.us-east-1.amazonaws.com/dev',
-  // },
+  playground: {
+    endpoint: 'https://e07p5jga5e.execute-api.us-east-1.amazonaws.com/dev',
+    subscriptionEndpoint: 'wss://qi4dcv7dn5.execute-api.us-east-1.amazonaws.com/dev',
+  },
+  connectionManager,
+  eventProcessor,
+  subscriptionManager,
   introspection: true,
 });
 
-export const handleHttp = server.createHandler({
+export const handleHttp = server.createHttpHandler({
   cors: {
     origin: '*',
     credentials: true,
   },
 });
+export const handleWebSocket = server.createWebSocketHandler();
+export const handleDynamoDBStream = server.createEventHandler();
